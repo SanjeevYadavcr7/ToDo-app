d5f9faf36913eb8152c2cd323c2d86e0c6b23350
Sanjeev Yadav <sanjeevyadavcr7@gmail.com>
2020-04-20 01:52:10 +0530

    Updates

---
+++ precache-manifest.c6469952b633b5ca1aeeb9eeb4e4321c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4a4432cdb2e93e528eb839aa2a02bd",
+    "url": "/SanjeevYadavcr7/ToDo-app/index.html"
+  },
+  {
+    "revision": "93e6d97a9d8a279aa9ff",
+    "url": "/SanjeevYadavcr7/ToDo-app/static/css/main.e0539b12.chunk.css"
+  },
+  {
+    "revision": "0a18d1502b468d1a4b93",
+    "url": "/SanjeevYadavcr7/ToDo-app/static/js/2.88c4bf8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SanjeevYadavcr7/ToDo-app/static/js/2.88c4bf8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93e6d97a9d8a279aa9ff",
+    "url": "/SanjeevYadavcr7/ToDo-app/static/js/main.f9e915e6.chunk.js"
+  },
+  {
+    "revision": "28e2ede856ec74723eef",
+    "url": "/SanjeevYadavcr7/ToDo-app/static/js/runtime-main.14058510.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SanjeevYadavcr7/ToDo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
